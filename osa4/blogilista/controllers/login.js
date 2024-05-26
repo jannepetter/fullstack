64899df3acf067cf2b46a10c961274e764ed7d69
jannepetter <peterdev80@gmail.com>
2024-05-26 19:25:19 +0300

    osa4

---
+++ osa4/blogilista/controllers/login.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const loginRouter = require("express").Router();
+const User = require("../models/user");
+
+loginRouter.post("/", async (request, response) => {
+  const { username, password } = request.body;
+  const user = await User.findOne({ username: username });
+  const passwordOK =
+    !!user?.password && bcrypt.compare(password, user.password);
+  if (user && passwordOK) {
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+    };
+    const options = {
+      expiresIn: 300,
+    };
+    const token = jwt.sign(userForToken, process.env.TOKEN_PASSWORD, options);
+    return response.status(200).send({ token });
+  }
+
+  return response.status(401).json({
+    error: "invalid username or password",
+  });
+});
+
+module.exports = loginRouter;
